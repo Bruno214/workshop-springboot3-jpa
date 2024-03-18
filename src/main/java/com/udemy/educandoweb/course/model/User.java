6e2b4892a05c5639af6abade6b870ef33b50b895
bruno.barcelos <bruno.barcelos@meta.com.br>
2024-03-18 15:59:06 -0300

    created conection h2 database and test profile and JPA

--- src/main/java/com/udemy/educandoweb/course/model/User.java
+++ src/main/java/com/udemy/educandoweb/course/model/User.java
@@ -1,9 +1,11 @@
 package com.udemy.educandoweb.course.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
-
+@Entity
+@Table(name = "tb_user")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
@@ -12,10 +14,13 @@ import java.io.Serializable;
 public class User implements Serializable {
 
   private static final long serialVersionUID=1L;
-
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
+  @Column(unique = true)
   private String email;
+  @Column(unique = true)
   private String phone;
   private String password;
 }
